76b17ae3f06877f84449f8445e19862ce7875dd7
ra1p3 <raipe.spamfilter@gmail.com>
2014-02-27 21:49:32 +0200

    NEW: Test cases

--- src/main/java/com/moonillusions/htmlUnitMatchers/matchers/AttributeCountMatcher.java
+++ src/main/java/com/moonillusions/htmlUnitMatchers/matchers/AttributeCountMatcher.java
@@ -1,5 +1,7 @@
 package com.moonillusions.htmlUnitMatchers.matchers;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.hamcrest.Description;
 import org.hamcrest.Factory;
 
@@ -8,7 +10,7 @@ import com.moonillusions.htmlUnitMatchers.core.ElementMatcher;
 import com.moonillusions.htmlUnitMatchers.utils.StringUtils;
 
 public class AttributeCountMatcher extends ElementMatcher<DomNode> {
-	
+
 	private int count;
 
 	public AttributeCountMatcher(int count) {
@@ -17,11 +19,11 @@ public class AttributeCountMatcher extends ElementMatcher<DomNode> {
 
 	@Override
 	protected void mismatch(DomNode item, Description mismatchDescription) {
-		mismatchDescription.appendText(StringUtils.print(item) + 
-				String.format(" has %s attributes instead of the expected %s",
-						attributeCount(item),
-						this.count));
-		
+		mismatchDescription.appendText(StringUtils.print(item)
+				+ String.format(
+						" has %s attributes instead of the expected %s",
+						attributeCount(item), this.count));
+
 	}
 
 	@Override
@@ -36,12 +38,33 @@ public class AttributeCountMatcher extends ElementMatcher<DomNode> {
 	@Override
 	protected void describe(Description arg0) {
 		arg0.appendText(String.format("Has %s attributes", this.count));
-		
+
 	}
-	
+
 	@Factory
 	public static AttributeCountMatcher hasAttributes(int count) {
-	    return new AttributeCountMatcher(count);
+		return new AttributeCountMatcher(count);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		if (obj == this) {
+			return true;
+		}
+		if (obj.getClass() != getClass()) {
+			return false;
+		}
+		AttributeCountMatcher rhs = (AttributeCountMatcher) obj;
+		return new EqualsBuilder().append(this.count, rhs.count).isEquals();
+
+	}
+
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder(17, 39).append(count).toHashCode();
 	}
 
 }
--- src/test/java/com/moonillusions/htmlUnitMatchers/matchers/AttributeCountMatcherTest.java
+++ src/test/java/com/moonillusions/htmlUnitMatchers/matchers/AttributeCountMatcherTest.java
@@ -1,16 +1,84 @@
 package com.moonillusions.htmlUnitMatchers.matchers;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+
 import org.junit.Test;
+
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.moonillusions.htmlUnitMatchers.TestUtils;
 
 import static com.moonillusions.htmlUnitMatchers.matchers.AttributeCountMatcher.hasAttributes;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class AttributeCountMatcherTest {
 
 	@Test
-	public void test() {
-		// TODO: missing tests
+	public void matchesNonZeroAttributes()
+			throws FailingHttpStatusCodeException, MalformedURLException,
+			IOException {
+		HtmlElement span = TestUtils
+				.createDomNode("<span attr1=1 attr2=2>foo</span>");
+		assertThat(span, hasAttributes(2));
+	}
+
+	@Test
+	public void matchesZeroAttributes() throws FailingHttpStatusCodeException,
+			MalformedURLException, IOException {
+		HtmlElement span = TestUtils.createDomNode("<span>foo</span>");
+		assertThat(span, hasAttributes(0));
+	}
+
+	@Test
+	public void mismatchIfTooFewAttributes()
+			throws FailingHttpStatusCodeException, MalformedURLException,
+			IOException {
+		HtmlElement span = TestUtils.createDomNode("<span attr1=1>foo</span>");
+		assertThat(hasAttributes(2).matches(span), equalTo(false));
+	}
+
+	@Test
+	public void mismatchIfTooManyAttributes()
+			throws FailingHttpStatusCodeException, MalformedURLException,
+			IOException {
+		HtmlElement span = TestUtils
+				.createDomNode("<span attr1=1 attr2=2>foo</span>");
+		assertThat(hasAttributes(1).matches(span), equalTo(false));
+	}
+
+	@Test
+	public void mismatchIfHaveUnExpected()
+			throws FailingHttpStatusCodeException, MalformedURLException,
+			IOException {
+		HtmlElement span = TestUtils
+				.createDomNode("<span attr1=1 attr2=2>foo</span>");
+		assertThat(hasAttributes(0).matches(span), equalTo(false));
+	}
+
+	@Test
+	public void mismatchIfHaveNoAttributes()
+			throws FailingHttpStatusCodeException, MalformedURLException,
+			IOException {
+		HtmlElement span = TestUtils.createDomNode("<span>foo</span>");
+		assertThat(hasAttributes(1).matches(span), equalTo(false));
+	}
+
+	@Test
+	public void describe() {
+		TestUtils.assertDescribeTo(hasAttributes(2), "Has 2 attributes");
+	}
+
+	@Test
+	public void mismatchDecriptionIfCountMismatch()
+			throws FailingHttpStatusCodeException, MalformedURLException,
+			IOException {
+		TestUtils
+				.assertDescribeMismatch(hasAttributes(2),
+						TestUtils.createDomNode("<span attr1=1>foo</span>"),
+						"HtmlSpan[<span attr1=1>foo</span>] has 1 attributes instead of the expected 2");
 	}
 
 	@Test
@@ -20,4 +88,10 @@ public class AttributeCountMatcherTest {
 		assertThat(test1, equalTo(test2));
 	}
 
+	@Test
+	public void equalsFailsIfCountMismatch() {
+		AttributeCountMatcher test1 = hasAttributes(2);
+		AttributeCountMatcher test2 = hasAttributes(3);
+		assertThat(test1, not(equalTo(test2)));
+	}
 }
