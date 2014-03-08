bf20dbc1d23b51d803b1d9f42d5ca11cb8138cd8
ra1p3 <raipe.spamfilter@gmail.com>
2014-03-08 15:55:57 +0200

    Missing matchers

--- src/test/java/com/moonillusions/htmlUnitMatchers/matchers/MatcherCollectionTest.java
+++ src/test/java/com/moonillusions/htmlUnitMatchers/matchers/MatcherCollectionTest.java
@@ -1,14 +1,19 @@
 package com.moonillusions.htmlUnitMatchers.matchers;
 
+import java.util.List;
+
 import org.hamcrest.Description;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import com.moonillusions.htmlUnitMatchers.TestUtils;
 import com.moonillusions.htmlUnitMatchers.core.ElementMatcher;
 import com.moonillusions.htmlUnitMatchers.core.MatchableExtractor;
 import com.moonillusions.htmlUnitMatchers.core.MatcherPair;
 
+import static ch.lambdaj.Lambda.extract;
+import static ch.lambdaj.Lambda.on;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.spy;
@@ -21,12 +26,17 @@ public class MatcherCollectionTest {
 	SimpleMatcher failMatcher1;
 	MatchableExtractor<String, String> extractor1;
 	MatchableExtractor<String, String> extractor2;
+	MatcherCollection<String> failingCollection;
+	MatcherCollection<String> matchingCollection;
 
 	@Before
 	public void setup() {
-		this.successMatcher1 = spy(new SimpleMatcher(true));
-		this.successMatcher2 = spy(new SimpleMatcher(true));
-		this.failMatcher1 = spy(new SimpleMatcher(false));
+		this.successMatcher1 = spy(new SimpleMatcher(true, "success1",
+				"fail on success1"));
+		this.successMatcher2 = spy(new SimpleMatcher(true, "success2",
+				"fail on success2"));
+		this.failMatcher1 = spy(new SimpleMatcher(false, "fail1",
+				"fail on fail1"));
 		this.extractor1 = new MatchableExtractor<String, String>() {
 			@Override
 			public String extract(String from) {
@@ -39,12 +49,7 @@ public class MatcherCollectionTest {
 				return "ext2";
 			}
 		};
-	}
-
-	@Test
-	public void executesAllMatchersOnSuccess() {
-
-		MatcherCollection<String> matchingCollection = new MatcherCollection<String>() {
+		matchingCollection = new MatcherCollection<String>() {
 			{
 				addMatcher(new MatcherPair<String, String>(successMatcher1,
 						extractor1));
@@ -53,17 +58,7 @@ public class MatcherCollectionTest {
 			}
 
 		};
-
-		assertThat(matchingCollection.matches("foobar"), equalTo(true));
-		verify(successMatcher1).match("ext1");
-		verify(successMatcher2).match("ext2");
-
-	}
-
-	@Test
-	public void stopMatchersOnFailure() {
-
-		MatcherCollection<String> failingCollection = new MatcherCollection<String>() {
+		failingCollection = new MatcherCollection<String>() {
 			{
 				addMatcher(new MatcherPair<String, String>(failMatcher1,
 						extractor2));
@@ -71,28 +66,66 @@ public class MatcherCollectionTest {
 						extractor1));
 			}
 		};
+	}
 
+	@Test
+	public void executesAllMatchersOnSuccess() {
+		assertThat(matchingCollection.matches("foobar"), equalTo(true));
+		@SuppressWarnings("unchecked")
+		List<SimpleMatcher> matchers = (List<SimpleMatcher>) (List<?>) extract(
+				matchingCollection.getMatchers(), on(MatcherPair.class)
+						.getMatcher());
+
+		assertThat(matchers.size(), equalTo(2));
+		assertThat((SimpleMatcher) matchers.get(0), equalTo(successMatcher1));
+		assertThat((SimpleMatcher) matchers.get(1), equalTo(successMatcher2));
+		verify(successMatcher1).match("ext1");
+		verify(successMatcher2).match("ext2");
+	}
+
+	@Test
+	public void stopMatchersOnFailure() {
 		assertThat(failingCollection.matches("foobar"), equalTo(false));
 		verify(failMatcher1).match("ext2");
 		verify(successMatcher1, Mockito.times(0)).match(Mockito.anyString());
 
 	}
 
-	// TODO: missing tests
-	// get failed matcher
-	// describe to
-	// mismatch desc
+	@Test
+	public void getFailingMatcher() {
+		assertThat(failingCollection.matches("foobar"), equalTo(false));
+		assertThat((SimpleMatcher) failingCollection.getFailedMatcher()
+				.getMatcher(), equalTo(failMatcher1));
+	}
+
+	@Test
+	public void describeTo() {
+		TestUtils.assertDescribeTo(failingCollection,
+				"Matches all:\n*fail1\n*success1");
+	}
+
+	@Test
+	public void mismatch() {
+		TestUtils.assertDescribeMismatch(failingCollection, "foobar",
+				"Failed on:\n*Fail on ext2 due to fail on fail1");
+	}
 
 	private class SimpleMatcher extends ElementMatcher<String> {
 
 		boolean match;
+		String description;
+		String mismatch;
 
-		public SimpleMatcher(boolean match) {
+		public SimpleMatcher(boolean match, String description, String mistmatch) {
 			this.match = match;
+			this.description = description;
+			this.mismatch = mistmatch;
 		}
 
 		@Override
 		protected void mismatch(String item, Description mismatchDescription) {
+			mismatchDescription.appendText(String.format(
+					"Fail on %s due to %s", item, this.mismatch));
 		}
 
 		@Override
@@ -102,6 +135,7 @@ public class MatcherCollectionTest {
 
 		@Override
 		protected void describe(Description arg0) {
+			arg0.appendText(description);
 		}
 
 	}
