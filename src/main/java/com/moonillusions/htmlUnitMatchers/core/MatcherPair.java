e847b8a09875f79e0497294a87278a2968e6d605
ra1p3 <raipe.spamfilter@gmail.com>
2014-03-08 21:51:53 +0200

    Remove unused code

--- src/main/java/com/moonillusions/htmlUnitMatchers/core/MatcherPair.java
+++ src/main/java/com/moonillusions/htmlUnitMatchers/core/MatcherPair.java
@@ -2,39 +2,33 @@ package com.moonillusions.htmlUnitMatchers.core;
 
 import org.hamcrest.Description;
 
-public class MatcherPair<T,F> {
+public class MatcherPair<T, F> {
 	private ElementMatcher<T> matcher;
-	private MatchableExtractor<T,F> extractor;
-	
-	public MatcherPair(ElementMatcher<T> matcher, MatchableExtractor<T,F> extractor) {
+	private MatchableExtractor<T, F> extractor;
+
+	public MatcherPair(ElementMatcher<T> matcher,
+			MatchableExtractor<T, F> extractor) {
 		this.matcher = matcher;
 		this.extractor = extractor;
 	}
+
 	public ElementMatcher<T> getMatcher() {
 		return matcher;
-	}
-	public void setMatcher(ElementMatcher<T> matcher) {
-		this.matcher = matcher;
 	}
 
 	public boolean isMatch(F from) {
 		return this.matcher.matches(extractor.extract(from));
 	}
-	public MatchableExtractor<T,F> getExtractor() {
-		return extractor;
-	}
+
 	public T getMatchable(F from) {
 		return this.extractor.extract(from);
 	}
-	
+
 	public void proxyDescribeMismatchSafely(F from,
 			Description mismatchDescription, int i) {
 		getMatcher().describeMismatchSafely(getMatchable(from),
 				mismatchDescription, i);
 
 	}
-	
-	
-	
 
 }
