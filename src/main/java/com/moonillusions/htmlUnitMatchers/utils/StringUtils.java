0ebfb8e52c66436008fe4a35358ca116929eed97
ra1p3 <raipe.spamfilter@gmail.com>
2014-03-09 20:50:17 +0200

    Housekeeping

--- src/main/java/com/moonillusions/htmlUnitMatchers/utils/StringUtils.java
+++ src/main/java/com/moonillusions/htmlUnitMatchers/utils/StringUtils.java
@@ -5,21 +5,19 @@ import com.gargoylesoftware.htmlunit.html.DomNode;
 public class StringUtils {
 
 	public static String print(DomNode element) {
-		StringBuffer asString  = new StringBuffer();
+		StringBuffer asString = new StringBuffer();
 		asString.append(element.getClass().getSimpleName());
 		asString.append("[");
-		asString.append(element.asXml()
-				.replaceAll("\"", "")
-				.replaceAll("\n", "")
-				.replaceAll(">\\s*", ">")
+		asString.append(element.asXml().replaceAll("\"", "")
+				.replaceAll("\n", "").replaceAll(">\\s*", ">")
 				.replaceAll("\\s*<", "<"));
 		asString.append("]");
 		return asString.toString();
 	}
-	
+
 	public static String indent(int count) {
 		String indent = "\n";
-		for(int i = 0; i < count; i++) {
+		for (int i = 0; i < count; i++) {
 			indent += "*";
 		}
 		return indent;
